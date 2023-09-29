a71c0401f806baf5b9ec54e420fbbcca7160ad87
Everton Balbinot <evebalbi23@gmail.com>
2023-09-29 14:01:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,405 @@
+const quotes = [
+{
+  "frase": "O Senhor é o meu pastor; nada me faltará.",
+  "autor": "Salmo 23:1"
+},
+{
+  "frase": "Amarás o teu próximo como a ti mesmo.",
+  "autor": "Mateus 22:39"
+},
+{
+  "frase": "No princípio, Deus criou os céus e a terra.",
+  "autor": "Gênesis 1:1"
+},
+{
+  "frase": "Eu sou o caminho, a verdade e a vida; ninguém vem ao Pai senão por mim.",
+  "autor": "João 14:6"
+},
+{
+  "frase": "Honra teu pai e tua mãe, para que se prolonguem os teus dias na terra que o Senhor teu Deus te dá.",
+  "autor": "Êxodo 20:12"
+},
+{
+  "frase": "Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna.",
+  "autor": "João 3:16"
+},
+{
+  "frase": "O temor do Senhor é o princípio da sabedoria; todos os que cumprem os seus preceitos revelam bom senso. Ele será louvado para sempre.",
+  "autor": "Salmos 111:10"
+},
+{
+  "frase": "Deus é amor, e quem permanece no amor permanece em Deus, e Deus nele.",
+  "autor": "1 João 4:16"
+},
+{
+  "frase": "O Senhor te abençoe e te guarde; o Senhor faça resplandecer o seu rosto sobre ti e te conceda graça; o Senhor volte para ti o seu rosto e te dê paz.",
+  "autor": "Números 6:24-26"
+},
+{
+  "frase": "Não se turbe o vosso coração; credes em Deus, crede também em mim.",
+  "autor": "João 14:1"
+},
+{
+  "frase": "Em todo o tempo ama o amigo, e na angústia se faz o irmão.",
+  "autor": "Provérbios 17:17"
+},
+{
+  "frase": "O Senhor é a minha luz e a minha salvação; a quem temerei? O Senhor é a força da minha vida; de quem me recearei?",
+  "autor": "Salmo 27:1"
+},
+{
+  "frase": "Todas as coisas contribuem juntamente para o bem daqueles que amam a Deus.",
+  "autor": "Romanos 8:28"
+},
+{
+  "frase": "Confia no Senhor de todo o teu coração e não te estribes no teu próprio entendimento.",
+  "autor": "Provérbios 3:5"
+},
+{
+  "frase": "O Senhor é a minha força e o meu escudo; nele o meu coração confia, nele fui socorrido; por isso, o meu coração exulta, e com o meu canto o louvarei.",
+  "autor": "Salmo 28:7"
+},
+{
+  "frase": "Tudo posso naquele que me fortalece.",
+  "autor": "Filipenses 4:13"
+},
+{
+  "frase": "O Senhor é a minha rocha, a minha fortaleza e o meu libertador; o meu Deus, o meu rochedo, em quem me refugio; o meu escudo, a força da minha salvação, o meu baluarte.",
+  "autor": "Salmo 18:2"
+},
+{
+  "frase": "Assim, eles já não são dois, mas sim uma só carne. Portanto, o que Deus uniu, ninguém o separe.",
+  "autor": "Mateus 19:6"
+},
+{
+  "frase": "Ouve, Israel, o Senhor, nosso Deus, é o único Senhor. Amarás o Senhor, teu Deus, de todo o teu coração, de toda a tua alma e de todo o teu entendimento.",
+  "autor": "Mateus 22:37-38"
+},
+{
+  "frase": "Não temas, porque eu sou contigo; não te assombres, porque eu sou o teu Deus; eu te fortaleço, e te ajudo, e te sustento com a minha destra fiel.",
+  "autor": "Isaías 41:10"
+},
+{
+  "frase": "O amor é paciente, o amor é bondoso. Não inveja, não se vangloria, não se orgulha.",
+  "autor": "1 Coríntios 13:4"
+},
+{
+  "frase": "Clama a mim, e responder-te-ei, e anunciar-te-ei coisas grandes e firmes que não sabes.",
+  "autor": "Jeremias 33:3"
+},
+{
+  "frase": "O meu Deus suprirá todas as vossas necessidades, segundo as suas riquezas na glória em Cristo Jesus.",
+  "autor": "Filipenses 4:19"
+},
+{
+  "frase": "Mas buscai primeiro o seu reino e a sua justiça, e todas estas coisas vos serão acrescentadas.",
+  "autor": "Mateus 6:33"
+},
+{
+  "frase": "Ainda que eu falasse a língua dos homens e dos anjos, e não tivesse amor, seria como o metal que soa ou como o sino que tine.",
+  "autor": "1 Coríntios 13:1"
+},
+{
+  "frase": "Tudo o que fizerdes, fazei-o de todo o coração, como para o Senhor e não para homens.",
+  "autor": "Colossenses 3:23"
+},
+{
+  "frase": "O homem de coração sábio é chamado prudente, e a doçura de lábios aumenta o aprendizado.",
+  "autor": "Provérbios 16:21"
+},
+{
+  "frase": "O Senhor é bom, é fortaleza no dia da angústia e conhece os que confiam nele.",
+  "autor": "Naum 1:7"
+},
+{
+  "frase": "E conhecereis a verdade, e a verdade vos libertará.",
+  "autor": "João 8:32"
+},
+{
+  "frase": "Entrega o teu caminho ao Senhor, confia nele, e o mais ele fará.",
+  "autor": "Salmo 37:5"
+},
+{
+  "frase": "O amigo ama em todos os momentos; é um irmão na adversidade.",
+  "autor": "Provérbios 17:17"
+},
+{
+  "frase": "Nunca se apartem de ti a benignidade e a fidelidade; ata-as ao pescoço; escreve-as na tábua do teu coração e acharás graça e bom entendimento aos olhos de Deus e dos homens.",
+  "autor": "Provérbios 3:3-4"
+},
+{
+  "frase": "O Senhor é o meu pastor; nada me faltará.",
+  "autor": "Salmo 23:1"
+},
+{
+  "frase": "Amarás o teu próximo como a ti mesmo.",
+  "autor": "Mateus 22:39"
+},
+{
+  "frase": "No princípio, Deus criou os céus e a terra.",
+  "autor": "Gênesis 1:1"
+},
+{
+  "frase": "Eu sou o caminho, a verdade e a vida; ninguém vem ao Pai senão por mim.",
+  "autor": "João 14:6"
+},
+{
+  "frase": "Honra teu pai e tua mãe, para que se prolonguem os teus dias na terra que o Senhor teu Deus te dá.",
+  "autor": "Êxodo 20:12"
+},
+{
+  "frase": "Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna.",
+  "autor": "João 3:16"
+},
+{
+  "frase": "O temor do Senhor é o princípio da sabedoria; todos os que cumprem os seus preceitos revelam bom senso. Ele será louvado para sempre.",
+  "autor": "Salmos 111:10"
+},
+{
+  "frase": "Deus é amor, e quem permanece no amor permanece em Deus, e Deus nele.",
+  "autor": "1 João 4:16"
+},
+{
+  "frase": "O Senhor te abençoe e te guarde; o Senhor faça resplandecer o seu rosto sobre ti e te conceda graça; o Senhor volte para ti o seu rosto e te dê paz.",
+  "autor": "Números 6:24-26"
+},
+{
+  "frase": "Não se turbe o vosso coração; credes em Deus, crede também em mim.",
+  "autor": "João 14:1"
+},
+{
+  "frase": "Em todo o tempo ama o amigo, e na angústia se faz o irmão.",
+  "autor": "Provérbios 17:17"
+},
+{
+  "frase": "O Senhor é a minha luz e a minha salvação; a quem temerei? O Senhor é a força da minha vida; de quem me recearei?",
+  "autor": "Salmo 27:1"
+},
+{
+  "frase": "Todas as coisas contribuem juntamente para o bem daqueles que amam a Deus.",
+  "autor": "Romanos 8:28"
+},
+{
+  "frase": "Confia no Senhor de todo o teu coração e não te estribes no teu próprio entendimento.",
+  "autor": "Provérbios 3:5"
+},
+{
+  "frase": "O Senhor é a minha força e o meu escudo; nele o meu coração confia, nele fui socorrido; por isso, o meu coração exulta, e com o meu canto o louvarei.",
+  "autor": "Salmo 28:7"
+},
+{
+  "frase": "Tudo posso naquele que me fortalece.",
+  "autor": "Filipenses 4:13"
+},
+{
+  "frase": "O Senhor é a minha rocha, a minha fortaleza e o meu libertador; o meu Deus, o meu rochedo, em quem me refugio; o meu escudo, a força da minha salvação, o meu baluarte.",
+  "autor": "Salmo 18:2"
+},
+{
+  "frase": "Assim, eles já não são dois, mas sim uma só carne. Portanto, o que Deus uniu, ninguém o separe.",
+  "autor": "Mateus 19:6"
+},
+{
+  "frase": "Ouve, Israel, o Senhor, nosso Deus, é o único Senhor. Amarás o Senhor, teu Deus, de todo o teu coração, de toda a tua alma e de todo o teu entendimento.",
+  "autor": "Mateus 22:37-38"
+},
+{
+  "frase": "Não temas, porque eu sou contigo; não te assombres, porque eu sou o teu Deus; eu te fortaleço, e te ajudo, e te sustento com a minha destra fiel.",
+  "autor": "Isaías 41:10"
+},
+{
+  "frase": "O amor é paciente, o amor é bondoso. Não inveja, não se vangloria, não se orgulha.",
+  "autor": "1 Coríntios 13:4"
+},
+{
+  "frase": "Clama a mim, e responder-te-ei, e anunciar-te-ei coisas grandes e firmes que não sabes.",
+  "autor": "Jeremias 33:3"
+},
+{
+  "frase": "O meu Deus suprirá todas as vossas necessidades, segundo as suas riquezas na glória em Cristo Jesus.",
+  "autor": "Filipenses 4:19"
+},
+{
+  "frase": "Mas buscai primeiro o seu reino e a sua justiça, e todas estas coisas vos serão acrescentadas.",
+  "autor": "Mateus 6:33"
+},
+{
+  "frase": "Ainda que eu falasse a língua dos homens e dos anjos, e não tivesse amor, seria como o metal que soa ou como o sino que tine.",
+  "autor": "1 Coríntios 13:1"
+},
+{
+  "frase": "Tudo o que fizerdes, fazei-o de todo o coração, como para o Senhor e não para homens.",
+  "autor": "Colossenses 3:23"
+},
+{
+  "frase": "O homem de coração sábio é chamado prudente, e a doçura de lábios aumenta o aprendizado.",
+  "autor": "Provérbios 16:21"
+},
+{
+  "frase": "O Senhor é bom, é fortaleza no dia da angústia e conhece os que confiam nele.",
+  "autor": "Naum 1:7"
+},
+{
+  "frase": "E conhecereis a verdade, e a verdade vos libertará.",
+  "autor": "João 8:32"
+},
+{
+  "frase": "Entrega o teu caminho ao Senhor, confia nele, e o mais ele fará.",
+  "autor": "Salmo 37:5"
+},
+{
+  "frase": "O amigo ama em todos os momentos; é um irmão na adversidade.",
+  "autor": "Provérbios 17:17"
+},
+{
+  "frase": "Nunca se apartem de ti a benignidade e a fidelidade; ata-as ao pescoço; escreve-as na tábua do teu coração e acharás graça e bom entendimento aos olhos de Deus e dos homens.",
+  "autor": "Provérbios 3:3-4"
+},
+{
+  "frase": "O Senhor é o meu pastor; nada me faltará.",
+  "autor": "Salmo 23:1"
+},
+{
+  "frase": "Amarás o teu próximo como a ti mesmo.",
+  "autor": "Mateus 22:39"
+},
+{
+  "frase": "No princípio, Deus criou os céus e a terra.",
+  "autor": "Gênesis 1:1"
+},
+{
+  "frase": "Eu sou o caminho, a verdade e a vida; ninguém vem ao Pai senão por mim.",
+  "autor": "João 14:6"
+},
+{
+  "frase": "Honra teu pai e tua mãe, para que se prolonguem os teus dias na terra que o Senhor teu Deus te dá.",
+  "autor": "Êxodo 20:12"
+},
+{
+  "frase": "Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna.",
+  "autor": "João 3:16"
+},
+{
+  "frase": "O temor do Senhor é o princípio da sabedoria; todos os que cumprem os seus preceitos revelam bom senso. Ele será louvado para sempre.",
+  "autor": "Salmos 111:10"
+},
+{
+  "frase": "Deus é amor, e quem permanece no amor permanece em Deus, e Deus nele.",
+  "autor": "1 João 4:16"
+},
+{
+  "frase": "O Senhor te abençoe e te guarde; o Senhor faça resplandecer o seu rosto sobre ti e te conceda graça; o Senhor volte para ti o seu rosto e te dê paz.",
+  "autor": "Números 6:24-26"
+},
+{
+  "frase": "Não se turbe o vosso coração; credes em Deus, crede também em mim.",
+  "autor": "João 14:1"
+},
+{
+  "frase": "Em todo o tempo ama o amigo, e na angústia se faz o irmão.",
+  "autor": "Provérbios 17:17"
+},
+{
+  "frase": "O Senhor é a minha luz e a minha salvação; a quem temerei? O Senhor é a força da minha vida; de quem me recearei?",
+  "autor": "Salmo 27:1"
+},
+{
+  "frase": "Todas as coisas contribuem juntamente para o bem daqueles que amam a Deus.",
+  "autor": "Romanos 8:28"
+},
+{
+  "frase": "Confia no Senhor de todo o teu coração e não te estribes no teu próprio entendimento.",
+  "autor": "Provérbios 3:5"
+},
+{
+  "frase": "O Senhor é a minha força e o meu escudo; nele o meu coração confia, nele fui socorrido; por isso, o meu coração exulta, e com o meu canto o louvarei.",
+  "autor": "Salmo 28:7"
+},
+{
+  "frase": "Tudo posso naquele que me fortalece.",
+  "autor": "Filipenses 4:13"
+},
+{
+  "frase": "O Senhor é a minha rocha, a minha fortaleza e o meu libertador; o meu Deus, o meu rochedo, em quem me refugio; o meu escudo, a força da minha salvação, o meu baluarte.",
+  "autor": "Salmo 18:2"
+},
+{
+  "frase": "Assim, eles já não são dois, mas sim uma só carne. Portanto, o que Deus uniu, ninguém o separe.",
+  "autor": "Mateus 19:6"
+},
+{
+  "frase": "Ouve, Israel, o Senhor, nosso Deus, é o único Senhor. Amarás o Senhor, teu Deus, de todo o teu coração, de toda a tua alma e de todo o teu entendimento.",
+  "autor": "Mateus 22:37-38"
+},
+{
+  "frase": "Não temas, porque eu sou contigo; não te assombres, porque eu sou o teu Deus; eu te fortaleço, e te ajudo, e te sustento com a minha destra fiel.",
+  "autor": "Isaías 41:10"
+},
+{
+  "frase": "O amor é paciente, o amor é bondoso. Não inveja, não se vangloria, não se orgulha.",
+  "autor": "1 Coríntios 13:4"
+},
+{
+  "frase": "Clama a mim, e responder-te-ei, e anunciar-te-ei coisas grandes e firmes que não sabes.",
+  "autor": "Jeremias 33:3"
+},
+{
+  "frase": "O meu Deus suprirá todas as vossas necessidades, segundo as suas riquezas na glória em Cristo Jesus.",
+  "autor": "Filipenses 4:19"
+},
+{
+  "frase": "Mas buscai primeiro o seu reino e a sua justiça, e todas estas coisas vos serão acrescentadas.",
+  "autor": "Mateus 6:33"
+},
+{
+  "frase": "Ainda que eu falasse a língua dos homens e dos anjos, e não tivesse amor, seria como o metal que soa ou como o sino que tine.",
+  "autor": "1 Coríntios 13:1"
+},
+{
+  "frase": "Tudo o que fizerdes, fazei-o de todo o coração, como para o Senhor e não para homens.",
+  "autor": "Colossenses 3:23"
+},
+{
+  "frase": "O homem de coração sábio é chamado prudente, e a doçura de lábios aumenta o aprendizado.",
+  "autor": "Provérbios 16:21"
+},
+{
+  "frase": "O Senhor é bom, é fortaleza no dia da angústia e conhece os que confiam nele.",
+  "autor": "Naum 1:7"
+},
+{
+  "frase": "E conhecereis a verdade, e a verdade vos libertará.",
+  "autor": "João 8:32"
+},
+{
+  "frase": "Entrega o teu caminho ao Senhor, confia nele, e o mais ele fará.",
+  "autor": "Salmo 37:5"
+},
+{
+  "frase": "O amigo ama em todos os momentos; é um irmão na adversidade.",
+  "autor": "Provérbios 17:17"
+},
+{
+  "frase": "Nunca se apartem de ti a benignidade e a fidelidade; ata-as ao pescoço; escreve-as na tábua do teu coração e acharás graça e bom entendimento aos olhos de Deus e dos homens.",
+  "autor": "Provérbios 3:3-4"
+}
+
+];
+
+const quoteText = document.getElementById("quote-text");
+const quoteAuthor = document.getElementById("quote-author");
+const newQuoteButton = document.getElementById("new-quote-button");
+
+function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+}
+
+function displayQuote() {
+    const randomQuote = getRandomQuote();
+    quoteText.textContent = randomQuote.frase;
+    quoteAuthor.textContent = `- ${randomQuote.autor}`;
+}
+
+newQuoteButton.addEventListener("click", displayQuote);
+displayQuote(); // Exibe uma citação aleatória ao carregar a página
